7f6fd2e22855e9a08526dae8bf4d112f4044bba4
Aminul Islam Rony <aminul.irony@gmail.com>
2025-01-04 01:02:56 +0600

    build version code increased

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.taman.silmebagcalculator"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
