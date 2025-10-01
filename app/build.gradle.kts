9dea22e4f61dfdeaefa1f7b1ffa000f4ab6e3a4b
Aminul Islam Rony <aminul.irony@gmail.com>
2025-10-02 01:36:50 +0600

    Refactor: Update application version code
    This commit updates the application version code from 5 to 6 in the `app/build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.taman.silmebagcalculator"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
